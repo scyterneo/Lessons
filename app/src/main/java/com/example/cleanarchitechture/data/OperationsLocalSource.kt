635d840b24c461ff118b236f5f1c3c442eb608bd
Andrei Kolubau <akolubau@lyft.com>
2021-04-11 11:53:23 +0300

    Lesson 26. Prepare Flow introduction

--- app/src/main/java/com/example/cleanarchitechture/data/OperationsLocalSource.kt
+++ app/src/main/java/com/example/cleanarchitechture/data/OperationsLocalSource.kt
@@ -6,25 +6,28 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.withContext
 
 class OperationsLocalSource : OperationsRepository {
 
+    private val _operationsFlow: MutableSharedFlow<List<Operation>> = MutableSharedFlow(replay = 1)
+
     private var operations = mutableListOf(Operation(1, 2, 3), Operation(3, 6, 9))
 
-    override suspend fun getOperations(): List<Operation> {
-        withContext(Dispatchers.IO) {
-            delay(3000)
+    override suspend fun getOperations() = _operationsFlow
+        .apply {
+            emit(operations)
         }
-        return operations
-    }
 
     override suspend fun addOperation(operation: Operation) {
         operations.add(operation)
+        _operationsFlow.emit(operations)
     }
 
     override suspend fun removeOperation(operation: Operation) {
         operations.remove(operation)
+        _operationsFlow.emit(operations)
     }
 }
--- app/src/main/java/com/example/cleanarchitechture/domain/OperationsUseCaseImpl.kt
+++ app/src/main/java/com/example/cleanarchitechture/domain/OperationsUseCaseImpl.kt
@@ -1,9 +1,11 @@
 package com.example.cleanarchitechture.domain
 
+import kotlinx.coroutines.flow.Flow
+
 class OperationsUseCaseImpl(
     private val operationsRepository: OperationsRepository
 ) : OperationsUseCase {
-    override suspend fun getOperations(): List<Operation> {
+    override suspend fun getOperations(): Flow<List<Operation>> {
         return operationsRepository.getOperations()
     }
 

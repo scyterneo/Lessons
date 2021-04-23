ea58e469a0840bc3836419fbecfa4c2206803d5a
Andrei Kolubau <akolubau@lyft.com>
2021-04-23 16:24:14 +0300

    Lesson 28. Practice

--- app/src/main/java/com/example/cleanarchitechture/domain/usecase/person/PersonsUseCaseImpl.kt
+++ app/src/main/java/com/example/cleanarchitechture/domain/usecase/person/PersonsUseCaseImpl.kt
@@ -1,21 +1,29 @@
 package com.example.cleanarchitechture.domain.usecase.person
 
+import com.example.cleanarchitechture.data.cloud.NetworkResult
 import com.example.cleanarchitechture.domain.entity.Person
 import io.reactivex.Flowable
 import kotlinx.coroutines.flow.Flow
 
-class PersonsUseCaseImpl(private val personsRepository: PersonsRepository): PersonsUseCase, EditPersonUseCase {
-    override fun getPersons(): Flow<List<Person>> =
+class PersonsUseCaseImpl(
+    private val personsRepository: PersonsRepository,
+    private val personsCloudRepository: PersonsCloudRepository
+) : PersonsUseCase, EditPersonUseCase {
+    override fun observePersons(): Flow<List<Person>> =
         personsRepository.getPersons()
 
     override fun getPersonsRX(): Flowable<List<Person>> =
         personsRepository.getPersonsRX()
 
-    override suspend fun addPerson(person: Person) {
-        personsRepository.addPerson(person)
+    override suspend fun addPerson(person: Person): NetworkResult<Person> {
+        return personsCloudRepository.addPerson(person)
     }
 
     override suspend fun deletePerson(person: Person) {
         personsRepository.deletePerson(person)
+    }
+
+    override suspend fun getPersons(): NetworkResult<List<Person>> {
+        return personsCloudRepository.getPersons()
     }
 }
--- app/src/main/java/com/example/cleanarchitechture/presentation/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/cleanarchitechture/presentation/viewmodel/MainViewModel.kt
@@ -1,23 +1,15 @@
 package com.example.cleanarchitechture.presentation.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cleanarchitechture.Dependencies
+import com.example.cleanarchitechture.data.cloud.NetworkResult
 import com.example.cleanarchitechture.domain.entity.Person
 import com.example.cleanarchitechture.domain.usecase.person.EditPersonUseCase
 import com.example.cleanarchitechture.domain.usecase.person.PersonsUseCase
 import com.example.cleanarchitechture.extensions.launch
-import io.reactivex.Scheduler
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class MainViewModel : ViewModel() {
 
@@ -37,11 +29,19 @@ class MainViewModel : ViewModel() {
         return topPersons
     }
 
+    private var error = MutableLiveData<String>()
+    fun getError(): LiveData<String> = error
+
     private val disposable = CompositeDisposable()
 
     private var _calculationState = MutableLiveData<CalculationState>(CalculationState.Free)
 
     val calculationState: LiveData<CalculationState> = _calculationState
+
+
+    init {
+        updatePersons()
+    }
 
     fun addPerson() {
         val rating = try {
@@ -51,8 +51,8 @@ class MainViewModel : ViewModel() {
         }
         val person = Person(name, rating)
         launch {
-            withContext(Dispatchers.IO) {
-                editPersonUseCase.addPerson(person)
+            processNetworkResult(editPersonUseCase.addPerson(person)) {
+                updatePersons()
             }
         }
     }
@@ -63,48 +63,67 @@ class MainViewModel : ViewModel() {
         }
     }
 
-    init {
-//        launch {
-//            personUseCase.getPersons().collect {
-//                persons.value = it
-//            }
-//        }
+    private fun <T> processNetworkResult(
+        networkResult: NetworkResult<T>,
+        action: (T) -> Unit
+    ) {
+        when (networkResult) {
+            is NetworkResult.Error -> {
+                error.value = networkResult.exception.message
+            }
+            is NetworkResult.Success -> {
+                action(networkResult.data)
+            }
+        }
+    }
 
-        val subscribe = personUseCase.getPersonsRX()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
+    private fun updatePersons() {
+        launch {
+            processNetworkResult(personUseCase.getPersons()) {
                 persons.value = it
             }
-
-        val subscribeTop = personUseCase.getPersonsRX()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .doOnNext {
-                Log.d("MainViewModel", Thread.currentThread().name)
-            }
-            .observeOn(Schedulers.io())
-            .map { persons ->
-                Log.d("MainViewModel", Thread.currentThread().name)
-                val sortedPersons = persons.sortedBy { it.rating }
-                val middleRating = try {
-                    sortedPersons[sortedPersons.size / 2].rating
-                } catch (exception: Exception) {
-                    0F
-                }
-                sortedPersons.filter { it.rating >= middleRating }
-            }
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe {
-                Log.d("MainViewModel", Thread.currentThread().name)
-                topPersons.value = it
-            }
-        disposable.add(subscribe)
-        disposable.add(subscribeTop)
+        }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        disposable.dispose()
-    }
+
+// init {
+
+
+//        val subscribe = personUseCase.getPersonsRX()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                persons.value = it
+//            }
+//
+//        val subscribeTop = personUseCase.getPersonsRX()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .doOnNext {
+//                Log.d("MainViewModel", Thread.currentThread().name)
+//            }
+//            .observeOn(Schedulers.io())
+//            .map { persons ->
+//                Log.d("MainViewModel", Thread.currentThread().name)
+//                val sortedPersons = persons.sortedBy { it.rating }
+//                val middleRating = try {
+//                    sortedPersons[sortedPersons.size / 2].rating
+//                } catch (exception: Exception) {
+//                    0F
+//                }
+//                sortedPersons.filter { it.rating >= middleRating }
+//            }
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe {
+//                Log.d("MainViewModel", Thread.currentThread().name)
+//                topPersons.value = it
+//            }
+//        disposable.add(subscribe)
+//        disposable.add(subscribeTop)
+//  }
+
+override fun onCleared() {
+    super.onCleared()
+    disposable.dispose()
+}
 }
